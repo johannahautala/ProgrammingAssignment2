7d28a78e72ac43c87c7375694213c5e670e7d149
johannahautala <once_for_ever@luukku.com>
2014-08-19 14:57:00 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## The following two functions make it faster to inverse a matrix. Inversing a 
+## huge matrix may be time consuming so if a matrix is inversed once, the 
+## result is stored. This result is then used instead of reinversing if the 
+## inverse is needed afterwards.
 
-makeCacheMatrix <- function(x = matrix()) {
+## The first function assigns a list of four simple functions to matrix. The
+## matrix is then handled through these functions in order to get or set the 
+## value or the inverse of the matrix.
 
+makeCacheMatrix <- function( Matr = matrix() ) {
+	
+	IM <- NULL  ## variable containing the inverse of the matrix
+	
+	## function to set the value of the matrix
+	setMatrix <- function( Matr2 ) {
+
+		Matr <<- Matr2
+		IM <<- NULL ## if matrix is changed, the previous inverse 
+				## must be nulled, of course
+	}
+	
+	## function to get the value of the matrix
+	getMatrix <- function() Matr
+	
+	## function to set the inverse of the matrix
+	setInverse <- function( InverseM ) IM <<- InverseM
+
+	## function to get the inverse of the matrix
+	getInverse <- function() IM
+
+	list( SetMatrix = setMatrix, GetMatrix = getMatrix,
+		SetInverse = setInverse, GetInverse = getInverse )
+	
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of the given matrix. If the inverse has
+## already been calculated, it is printed. If not, it is calculated and then
+## printed.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function( Matr, ... ) {
+        
+	IM <- Matr$GetInverse()
+
+	if( is.null( IM ) ) { ## i.e. the inverse has not ever been calculated
+		
+		data <- Matr$GetMatrix()
+
+		IM <- solve( data, ... )
+
+		Matr$SetInverse( IM )
+	} else {
+
+		message( "cached inverse" )
+	}
+		
+	IM
+
 }
+
